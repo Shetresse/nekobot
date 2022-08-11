2b42dc0acbfc44101534b070b85fb5fc1d2df8bf
Shetresse <106036221+Shetresse@users.noreply.github.com>
2022-08-11 09:32:31 -0500

    Update descargas-imagen.js

--- plugins/descargas-imagen.js
+++ plugins/descargas-imagen.js
@@ -7,7 +7,6 @@ let link = image
 conn.sendHydrated(m.chat, `💞 𝙍𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤 | 𝙍𝙚𝙨𝙪𝙡𝙩: ${text}`, `𝙂𝙤𝙤𝙜𝙡𝙚 | ${wm}`, link, link, '☘️ 𝙐𝙍𝙇', null, null, [
 ['🔄 𝙎𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 | 𝙉𝙚𝙭𝙩', `/imagen ${text}`],
 ['🔍 𝙋𝙞𝙣𝙩𝙚𝙧𝙚𝙨𝙩 ', `#pinterest ${text}`],
-['🐈 𝙈𝙚𝙣𝙪', `.menu`],  
 ], m)
 }
 handler.help = ['gimage <query>', 'imagen <query>']
